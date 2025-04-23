3188d31a5cf50e33cd289a13e32b5ac662a6d4ab
jncnt <jincent.caritan.23@usjr.edu.ph>
2025-04-23 22:39:21 +0800

    comment

--- src/FinalAct1.java
+++ src/FinalAct1.java
@@ -8,8 +8,8 @@ public class FinalAct1 {
             throw new IllegalArgumentException("Strings must be of equal length.");
         }
 
-        int matches = 0;
-        int total = str1.length();
+        // int matches = 0;
+        // int total = str1.length();
 
         for (int i = 0; i < total; i++) {
             if (str1.charAt(i) == str2.charAt(i)) {
